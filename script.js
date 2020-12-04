563ba72f755ee86f13f896d1c31b114e0886f054
unknown <you@example.com>
2020-12-04 13:23:35 +0300

    добавяются новые блоки

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 'use stric'
+"use strict"
 
 let cvs = document.querySelector('#canvas');
 let ctx = cvs.getContext('2d');
@@ -21,27 +21,57 @@ pipeBottom.src = 'img/pipeBottom.png';
 
 let gap = 90;                // отступы между блоков
 
+
+document.addEventListener('keydown' , moveUp);
+
+function moveUp(){
+    yPos -=30;    
+}
+
+
+// создание блоков
+
+let pipe = [];
+
+pipe[0] = {
+    x : cvs.width ,
+    y : 0
+}
+
 // позиция птички
 
 let xPos = 10;
 let yPos = 150;
-let grav = 2;
+let grav = 1;
 
 function draw(){             // рисуем в canvas
     ctx.drawImage(bg, 0, 0);
 
-    ctx.drawImage(pipeUp , 100, 0);
-    ctx.drawImage(pipeBottom , 100, 0 + pipeUp.height + gap);
+    for (let i = 0; i< pipe.length ; i++ ) {
+        ctx.drawImage(pipeUp , pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom , pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
-    // ctx.drawImage(fg, 0, 400);
+        pipe[i].x--;
+        
+        if (pipe[i].x == 125) {
+            pipe.push({
+                x : cvs.width ,
+                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+    }
+    
+
+    ctx.drawImage(fg, 0, 400);
     ctx.drawImage(bird , xPos, yPos);
     
     yPos += grav;                      // меняем позицию птички
     requestAnimationFrame(draw);
 
 }
-
-
 pipeBottom.onload = draw;
+
+
+
 
 
