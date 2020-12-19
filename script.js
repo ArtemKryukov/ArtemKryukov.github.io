d11744aa6cc788424475e08ef5d818e1174f451c
unknown <you@example.com>
2020-12-19 14:57:05 +0300

    Финальная версия

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ let ctx = cvs.getContext('2d');
 
 
 
-let bird = new Image();
-let bg = new Image();
-let fg = new Image();
-let pipeUp = new Image();
-let pipeBottom = new Image();
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeUp = new Image();
+const pipeBottom = new Image();
+const gameOver = new Image();
+const startImg = new Image();
+
 
 bird.src = 'img/bird.png';
 bg.src = 'img/bg.png';
 fg.src = 'img/fg.png';
 pipeUp.src = 'img/pipeUp.png';
 pipeBottom.src = 'img/pipeBottom.png';
+gameOver.src = 'img/gameover.png';
+startImg.src = 'img/strat.jpg'
 
 
-
-let gap = 90;                // отступы между блоков
-
-
+// при нажатии на кнопку
 document.addEventListener('keydown' , moveUp);
-
 function moveUp(){
     yPos -=30;    
 }
 
 
 // создание блоков
-
 let pipe = [];
 
 pipe[0] = {
@@ -42,33 +42,68 @@ pipe[0] = {
 
 let xPos = 10;
 let yPos = 150;
-let grav = 1;
 
-function draw(){             // рисуем в canvas
+const grav = 0.8;
+const gap = 120;                // отступы между блоков
+let score = 0;
+
+function start (callback) {
+    ctx.drawImage(startImg , 0 , 0);
+    alert('Нажиймай на пробел и пролетай между блоков');
+    callback();
+}
+
+
+function draw(){   
+
+    // рисуем в canvas
     ctx.drawImage(bg, 0, 0);
 
+        // создание блоков
     for (let i = 0; i< pipe.length ; i++ ) {
+        
         ctx.drawImage(pipeUp , pipe[i].x, pipe[i].y);
         ctx.drawImage(pipeBottom , pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
         pipe[i].x--;
         
-        if (pipe[i].x == 125) {
+        if (pipe[i].x == 93) {
             pipe.push({
                 x : cvs.width ,
                 y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
             });
         }
+
+    // столконвения птички
+        if((xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height
+            || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height)){
+                location.reload();
+                return alert('Game over! Вы набрали ' + score + ' очков. Нажмите Ок что-бы попробовать еще раз.' );
+                
+        }
+
+        if(pipe[i].x == 5){
+            score++;
+            
+        }
     }
-    
+
 
     ctx.drawImage(fg, 0, 400);
     ctx.drawImage(bird , xPos, yPos);
     
     yPos += grav;                      // меняем позицию птички
+
+    // score
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : "+score,10,cvs.height-20);
+
     requestAnimationFrame(draw);
 
 }
+
+start(draw);
 pipeBottom.onload = draw;
 
 
